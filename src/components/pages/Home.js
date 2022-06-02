9a26e6b0aadd030050e607759725a960b77b1874
md.jewel <md.jewelmia52@gmail.com>
2022-06-02 15:00:11 +0600

    home page correctoin

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,9 @@ export default function Home() {
     return (
         <>
         <div className={`container  ${classes.containerBg}`}>
-            <div className={classes.deviceCount}>3</div>
+            <div className={classes.deviceCount}>3
+               <p className={classes.devicesOnline}>DIVICES ONLINE</p>
+            </div>
         
         </div>
          </>
